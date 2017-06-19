aeb232cbd47da1002164910f6de8e0b96f270d57
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-06-18 22:25:40 -0500

    added

--- Labs/Lab2-JHHernandez.sql
+++ Labs/Lab2-JHHernandez.sql
@@ -1,4 +1,3 @@
------------------------------
 /* Jesus Hilario Hernandez */
 /* Lab No. 2               */
 /* June 18, 2017           */
@@ -19,17 +18,115 @@ CREATE TABLE movies
 );
 --2. Full movies table with following information.
 INSERT INTO movies
-VALUES
-(DEFAULT, 'The Shawshank Redemption'        , 'Drama'   , 142   , '1994', 'Tim Robbins'         , '9.2'),
-(DEFAULT, 'The Godfather'                   , 'Drama'   , 175   , '1972', 'Marlon Brando'       , '9.2'),
-(DEFAULT, 'Pulp Fiction'                    , 'Thriller', 154   , '1994', 'John Travolta'       , '8.9'),
-(DEFAULT, 'Schindler\'s List'               , 'Drama'   , 195   , '1993', 'Liam Neeson'         , '8.9'),
-(DEFAULT, '12 Angry Men'                    , 'Drama'   , 96    , '1957', 'Martin Balsam'       , '8.9'),
-(DEFAULT, 'One Flew Over the Cuckoo\'s Nest', 'Drama'   , 133   , '1975', 'Jack Nicholson'      , '8.8'),
-(DEFAULT, 'The Dark Knight'                 , 'Action'  , 152   , '2008', 'Christian Bale'      , '8.8'),
-(DEFAULT, 'The Lord of the Rings: The Return of the King', 'Action', 201, '2003', 'Elijah Wood' , '8.8'),
-(DEFAULT, 'Star Wars'                       , 'Action'  , 121   , '1977', 'Mark Hamill'         , '8.8'),
-(DEFAULT, 'Casablanca'                      , 'Drama'   , 102   , '1942', 'Humphrey Bogart'     , '8.8');
+VALUES (
+          1,
+          'The Shawshank Redemption',
+          'Drama',
+          142,
+          '1994',
+          'Tim Robbins',
+          '9.2'
+        );
+
+INSERT INTO movies
+VALUES (
+          2,
+          'The Godfather',
+          'Drama',
+          175,
+          '1972',
+          'Marlon Brando',
+          '9.2'
+      );
+
+INSERT INTO movies
+VALUES (
+          3,
+          'Pulp Fiction' ,
+          'Thriller',
+          154,
+          '1994',
+          'John Travolta',
+          '8.9'
+      );
+
+INSERT INTO movies
+VALUES (
+          4,
+          'Schindler''s List',
+          'Drama',
+          195,
+          '1993',
+          'Liam Neeson',
+          '8.9'
+      );
+
+INSERT INTO movies
+VALUES (
+          5,
+          '12 Angry Men',
+          'Drama',
+          96,
+          '1957',
+          'Martin Balsam',
+          '8.9'
+      );
+
+INSERT INTO movies
+VALUES (
+          6,
+          'One Flew Over the Cuckoo''s Nest',
+          'Drama',
+          133,
+          '1975',
+          'Jack Nicholson',
+          '8.8'
+      );
+
+INSERT INTO movies
+VALUES (
+          7,
+          'The Dark Knight',
+          'Action',
+          152,
+          '2008',
+          'Christian Bale',
+          '8.8'
+      );
+
+INSERT INTO movies
+VALUES (
+          8,
+          'The Lord of the Rings: The Return of the King',
+          'Action',
+          201,
+          '2003',
+          'Elijah Wood',
+          '8.8'
+      );
+
+INSERT INTO movies
+VALUES (
+          9,
+          'Star Wars',
+          'Action',
+          121,
+          '1977',
+          'Mark Hamill',
+          '8.8'
+      );
+
+INSERT INTO movies
+VALUES (
+          10,
+          'Casablanca',
+          'Drama',
+          102,
+          '1942',
+          'Humphrey Bogart',
+          '8.8'
+      );
+
 
 --3. List the columns from table movies.
 DESCRIBE movies;
@@ -44,3 +141,23 @@ SELECT * FROM movies ORDER BY year DESC;
 SELECT * FROM movies ORDER BY category, rating;
 
 --7. Show MovieName for movies with Lord in the title.
+SELECT * FROM movies
+WHERE MovieName LIKE '%Lord%';
+
+--8. Change the rating for Lord of the Rings to 5.0
+UPDATE movies
+SET rating = '5.0'
+WHERE MovieName = 'The Lord of the Rings: The Return of the King';
+
+--9. Change the rating for Pulp Fiction to 9.5
+UPDATE movies
+SET rating = '9.5'
+WHERE MovieName = 'Pulp Fiction';
+
+-- 10. Show the table of contents in order by rating
+SELECT *
+FROM movies
+ORDER BY rating;
+
+--11. Delete the table and its contents
+DROP TABLE movies;
